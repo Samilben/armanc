a65a370455beb99b2be8144bab80573d88780a90
Samilben <108641119+Samilben@users.noreply.github.com>
2022-07-06 19:50:23 +0300

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -198,7 +198,7 @@ async def play(_, message: Message):
             keyboard = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton("🚀 Bilgi", url=f"https://t.me/Masal_Muzik_Bot"),
+                InlineKeyboardButton("🚀 Bilgi", url=f"https://t.me/masalmuzikBot"),
                 InlineKeyboardButton("📣 Kanal", url=f"https://t.me/umudumuzukirdilar"),
             ],[
                 InlineKeyboardButton("📲 Kapat", callback_data="cls"),
@@ -263,7 +263,7 @@ async def play(_, message: Message):
         keyboard = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton("🚀 Bilgi", url=f"https://t.me/Masal_Muzik_Bot"),
+                InlineKeyboardButton("🚀 Bilgi", url=f"https://t.me/masalmuzikBot"),
                 InlineKeyboardButton("📣 Kanal", url=f"https://t.me/umudumuzukirdilar"),
             ],[
                 InlineKeyboardButton("📲 Kapat", callback_data="cls"),
